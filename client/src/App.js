c449e35ab76f103b34fde2935ba94f49ae25ca0d
chrisdalao <chrisdalao@gmail.com>
2019-06-11 14:52:08 -0700

    completed all stretch problems - Apple Nav Bar is a WIP

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
         />
         <Route
           path="/movies/:movieId"
-          component={Movie}
+          render={(props) => <Movie {...props} addToSavedList={this.addToSavedList} />}
         />
       </div>
     );
